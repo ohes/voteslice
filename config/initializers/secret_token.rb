0efea7ed877fea08c554b42ce14c7d751efa5682
Esther Oh <estheroh@gmail.com>
2015-08-03 02:45:17 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Voteslice::Application.config.secret_key_base = 'a571ba306213fc24b5b7d39775638ea6d135e90bbb6c115590d4250926b41cfa14cc4f312800f31ff487f2c093932365b2f0e025e62d92688f1167df7e5f53e5'
